5fe7e64d86e0094a2492c82b5365e924b122c207
nitinbly84 <nitin.bly@gmail.com>
2020-02-26 07:40:00 +0530

    Updated to add one more case - if no kids available

--- src/amphoraFamily/Executor.java
+++ src/amphoraFamily/Executor.java
@@ -1,6 +1,7 @@
 package amphoraFamily;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,7 +12,7 @@ import java.util.stream.Collectors;
  */
 public class Executor {
 	private static FamilyTree amphora;
-	
+
 	public static void main(String[] args) {
 		setup();
 		System.out.println("Initial Number of members in Family : " + amphora.getCountOfMembers());
@@ -34,7 +35,7 @@ public class Executor {
 		parent2.setRelation(new Relation(parent2, member13, RelationType.PARENT));
 		amphora.addMember(member13);
 		System.out.println("Number of members in Family after adding Meher Das : " + amphora.getCountOfMembers());
-		
+
 		// Adding grand parent
 		System.out.println("\n---------Adding Grand parent Sushila Kumari as spouse of Sunil Jamuna Das in the family-----------");
 		Member member4 = new Member("Sushila Kumari");
@@ -47,22 +48,22 @@ public class Executor {
 		member4.setRelation(new Relation(member4, kid, RelationType.PARENT));
 		kid.setRelation(new Relation(kid, member4, RelationType.KIDS));
 		amphora.addMember(member4);
-		
+
 		System.out.println("Number of members in Family after adding Sushila Kumari : " + amphora.getCountOfMembers());
-		
+
 		// Printing the family members' names in the increasing order of their age
-		System.out.println("\n--------Members of the family in the sorted oredr of their age---------");
+		System.out.println("\n--------Members of the family in the sorted order of their age---------");
 		Collections.sort(amphora.getListOfMembers());
 		amphora.getListOfMembers().stream()
-		                          .sorted()
-		                          .map(mem -> mem.getName())
-		                          .collect(Collectors.toList())
-		                          .forEach(System.out::println);
-		
+		.sorted()
+		.map(mem -> mem.getName())
+		.collect(Collectors.toList())
+		.forEach(System.out::println);
+
 		// Q3 : Can be done using the binary search to find the position of insertion
 		// But this will not be an accepted way to add a member in the family & member
 		// must be added in a manner like shown above.
-		
+
 		// Name of GrandChildren
 		System.out.println("\n---------Grand Kids of Jamuna Das--------------");
 		Member greatGrandFather = new Member("Jamuna Das"); //GreatGrandParent
@@ -71,9 +72,16 @@ public class Executor {
 		greatGrandFather = amphora.getMember(greatGrandFather).orElse(null);
 		List<Member> members = amphora.getMemberOfRelation(greatGrandFather, RelationType.GRANDKIDS);
 		members.stream()
+		.map(m -> m.getName())
+		.forEach(System.out::println);
+
+		// Name of Children
+		System.out.println("\n---------Kids of Jamuna Das--------------");
+		members = amphora.getMemberOfRelation(greatGrandFather, RelationType.KIDS);
+		members.stream()
 			   .map(m -> m.getName())
 		       .forEach(System.out::println);
-		
+
 		System.out.println("\n---------Grand Kids of Amar Jamuna Das--------------");
 		Member grandFather = new Member("Amar Januna Das"); //GrandParent
 		grandFather.setAge(81);
@@ -81,9 +89,9 @@ public class Executor {
 		grandFather = amphora.getMember(grandFather).orElse(null);
 		members = amphora.getMemberOfRelation(grandFather, RelationType.GRANDKIDS);
 		members.stream()
-			   .map(m -> m.getName())
-		       .forEach(System.out::println);
-		
+		.map(m -> m.getName())
+		.forEach(System.out::println);
+
 		System.out.println("\n---------Grand Parents of Mridul Das----------------");
 		Member kids = new Member("Mridul Das"); //Kids
 		kids.setAge(27);
@@ -91,12 +99,23 @@ public class Executor {
 		kids = amphora.getMember(kids).orElse(null);
 		List<Member> grandParents = amphora.getMemberOfRelation(kids, RelationType.GRANDPARENT);
 		grandParents.stream()
-		       .map(m -> m.getName())
-		       .forEach(System.out::println);
-		
+		.map(m -> m.getName())
+		.forEach(System.out::println);
+
+		System.out.println("\n-------Searching the kids of Sanchita Das-----------");
+		Member memberNoKid = new Member("Sanchita Das"); //Parent & kid of Sunil
+		memberNoKid.setAge(58);
+		memberNoKid.setAlive(true);
+		Member parent = amphora.getMember(memberNoKid).orElse(null);
+		List<Member> children = amphora.getMemberOfRelation(parent, RelationType.KIDS);
+		if(children.isEmpty())
+			System.out.println(parent.getName() + " has no kid....");
+		children.stream()
+		        .map(m -> m.getName())
+		        .forEach(System.out::println);
 		
 	}
-	
+
 	private static void setup() {
 		List<Member> members = new ArrayList<>();
 		Member member1 = new Member("Jamuna Das"); //GreatGrandParent
@@ -108,9 +127,9 @@ public class Executor {
 		Member member3 = new Member("Sunil Jamuna Das"); //GrandParent
 		member3.setAge(82);
 		member3.setAlive(true);
-//		Member member4 = new Member("Sushila Kumari"); //GrandParent
-//		member4.setAge(80);
-//		member4.setAlive(true);
+		//		Member member4 = new Member("Sushila Kumari"); //GrandParent
+		//		member4.setAge(80);
+		//		member4.setAlive(true);
 		Member member5 = new Member("Amar Januna Das"); //GrandParent
 		member5.setAge(81);
 		member5.setAlive(false);
@@ -138,7 +157,7 @@ public class Executor {
 		members.add(member1);
 		members.add(member2);
 		members.add(member3);
-//		members.add(member4);
+		//		members.add(member4);
 		members.add(member5);
 		members.add(member6);
 		members.add(member7);
@@ -147,10 +166,10 @@ public class Executor {
 		members.add(member10);
 		members.add(member11);
 		members.add(member12);
-		
+
 		amphora = new FamilyTree(member1);
 		amphora.addMembers(members);
-		
+
 		//Setting Relations & adding each member to family
 		member1.setRelation(new Relation(member1, member2, RelationType.SPOUSE));
 		member2.setRelation(new Relation(member2, member1, RelationType.SPOUSE));
@@ -162,14 +181,14 @@ public class Executor {
 		member3.setRelation(new Relation(member3, member2, RelationType.KIDS));
 		member2.setRelation(new Relation(member2, member5, RelationType.PARENT));
 		member5.setRelation(new Relation(member5, member2, RelationType.KIDS));
-//		member3.setRelation(new Relation(member3, member4, RelationType.SPOUSE));
-//		member4.setRelation(new Relation(member4, member3, RelationType.SPOUSE));
+		//		member3.setRelation(new Relation(member3, member4, RelationType.SPOUSE));
+		//		member4.setRelation(new Relation(member4, member3, RelationType.SPOUSE));
 		member5.setRelation(new Relation(member5, member6, RelationType.SPOUSE));
 		member6.setRelation(new Relation(member6, member5, RelationType.SPOUSE));
 		member3.setRelation(new Relation(member3, member7, RelationType.PARENT));
-//		member4.setRelation(new Relation(member4, member7, RelationType.PARENT));
+		//		member4.setRelation(new Relation(member4, member7, RelationType.PARENT));
 		member7.setRelation(new Relation(member7, member3, RelationType.KIDS));
-//		member7.setRelation(new Relation(member7, member4, RelationType.KIDS));
+		//		member7.setRelation(new Relation(member7, member4, RelationType.KIDS));
 		member7.setRelation(new Relation(member7, member8, RelationType.SPOUSE));
 		member8.setRelation(new Relation(member8, member7, RelationType.SPOUSE));
 		member5.setRelation(new Relation(member5, member9, RelationType.PARENT));
